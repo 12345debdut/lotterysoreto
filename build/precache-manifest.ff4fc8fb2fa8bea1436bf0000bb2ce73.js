f74c2d7b4b398907e08ed97a120e701256c87295
12345debdut <38096408+12345debdut@users.noreply.github.com>
2019-12-31 13:22:00 +0530

    Add files via upload

---
+++ build/precache-manifest.ff4fc8fb2fa8bea1436bf0000bb2ce73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdd973b20e9fdc30ab4086ee7fc9843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f22ce7d7453fa2278e",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "3ae6d158c594a668b813",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "59f22ce7d7453fa2278e",
+    "url": "/static/js/2.92b4aea6.chunk.js"
+  },
+  {
+    "revision": "86a0bbfec78d92cb22ee4fb9c8c7a384",
+    "url": "/static/js/2.92b4aea6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ae6d158c594a668b813",
+    "url": "/static/js/main.80548b69.chunk.js"
+  },
+  {
+    "revision": "6eecd15ff34a9459eb6e",
+    "url": "/static/js/runtime-main.2b09a910.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
